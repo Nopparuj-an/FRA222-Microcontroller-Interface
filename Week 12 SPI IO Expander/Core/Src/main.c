c83e22c29cbeeedb3e12a34fb5eaca2a26330e57
Nopparuj-an <47713359+Nopparuj-an@users.noreply.github.com>
2023-05-11 12:44:15 +0700

    week 12

--- Week 12 SPI IO Expander/Core/Src/main.c
+++ Week 12 SPI IO Expander/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -45,6 +45,9 @@ UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 
+uint8_t SPIRx[10];
+uint8_t SPITx[10];
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -53,6 +56,9 @@ static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_SPI3_Init(void);
 /* USER CODE BEGIN PFP */
+
+void SPITxRx_Setup();
+void SPITxRx_readIO();
 
 /* USER CODE END PFP */
 
@@ -93,16 +99,18 @@ int main(void)
   MX_SPI3_Init();
   /* USER CODE BEGIN 2 */
 
+	SPITxRx_Setup();
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+	while (1) {
+		SPITxRx_readIO();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
+	}
   /* USER CODE END 3 */
 }
 
@@ -269,6 +277,29 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
+void SPITxRx_Setup() {
+	//CS pulse
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, 0); // CS select
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, 1); // CS deselect
+	HAL_Delay(1);
+}
+
+void SPITxRx_readIO() {
+	if (HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_2)) {
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, 0); // CS select
+		SPITx[0] = 0b01000001;
+		SPITx[1] = 0x12;
+		SPITx[2] = 0;
+		SPITx[3] = 0;
+		HAL_SPI_TransmitReceive_IT(&hspi3, SPITx, SPIRx, 4);
+	}
+}
+
+void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi) {
+	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, 1); // CS deselect
+}
+
 /* USER CODE END 4 */
 
 /**
@@ -278,11 +309,10 @@ static void MX_GPIO_Init(void)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
   /* USER CODE END Error_Handler_Debug */
 }
 
